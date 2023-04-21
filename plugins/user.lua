14cdfd6519444a1363cdf4e9acaa9962cb8da32f
William Vandebelt <wvandebelt@salesforce.com>
2023-04-21 17:41:04 -0400

    Hop setup

--- plugins/user.lua
+++ plugins/user.lua
@@ -11,8 +11,10 @@ return {
   -- },
   {
     "phaazon/hop.nvim",
+    branch = 'v2', -- optional but strongly recommended
     config = function()
-      require("hop").setup()
+      -- you can configure Hop the way you like here; see :h hop-config
+      require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
     end,
-  }
+  },
 }
