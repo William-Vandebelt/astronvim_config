267bfe6a3f8ca7ae9501391842097254f6400725
William Vandebelt <wvandebelt@salesforce.com>
2023-04-21 17:58:52 -0400

    Hop setup

--- init.lua
+++ init.lua
@@ -22,8 +22,8 @@ return {
       "phaazon/hop.nvim",
       as "hop",
       config = function()
-      -- you can configure Hop the way you like here; see :h hop-config
-      require'hop'.setup()
+        -- you can configure Hop the way you like here; see :h hop-config
+        require("hop").setup {}
       end,
     },
   }
