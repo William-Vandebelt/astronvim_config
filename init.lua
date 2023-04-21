30e698c6cf908eee897ae6f69e514bec41b9b0dd
William Vandebelt <wvandebelt@salesforce.com>
2023-04-21 18:00:51 -0400

    format error

--- init.lua
+++ init.lua
@@ -19,11 +19,10 @@ return {
 
   plugins = {
     {
-      "phaazon/hop.nvim",
-      as "hop",
+      'phaazon/hop.nvim',
+      branch = 'v2',
       config = function()
-        -- you can configure Hop the way you like here; see :h hop-config
-        require("hop").setup {}
+        require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
       end,
     },
   },
